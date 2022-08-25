e2faf8c9fec88bfd54730e6b89b720203216d13e
VictorCraciun <craciun.victor.tudor@gmail.com>
2022-08-25 19:25:24 +0100

    Update demo_mongodb_createcollection.js

--- demo_mongodb_createcollection.js
+++ demo_mongodb_createcollection.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.4";
+var url = "mongodb://0.0.0.0:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
