7ca272c45fc8a92a7311042e326d302462159c13
VictorCraciun <craciun.victor.tudor@gmail.com>
2022-08-29 19:33:29 +0100

    Update demo_mongodb_insert.js

--- demo_mongodb_insert.js
+++ demo_mongodb_insert.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://victor:<Teodora13>@cluster0.ungxvbj.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("Collection created!")
+  if (err) throw err;
+  console.log("Conection OK!")
   client.close();
 });
