61851761d8fd2cf096607df45b3a43203fb5fe60
VictorCraciun <craciun.victor.tudor@gmail.com>
2022-08-29 19:20:06 +0100

    Update demo_mongodb_insert.js

--- demo_mongodb_insert.js
+++ demo_mongodb_insert.js
@@ -1,14 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const url = "mongodb+srv://victor:<Teodora13>@cluster0.ungxvbj.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const uri = "mongodb+srv://victor:<Teodora13>@cluster0.ungxvbj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  var myobj = { name: "Company Inc", address: "Highway 37" };
-  const collection = client.db("test").collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    client.close()
-  });
+  const collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  ;
+  client.close();
 });
