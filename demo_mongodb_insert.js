ee2ea5f918e164a47f81ac3513458e2d7fa3b948
VictorCraciun <craciun.victor.tudor@gmail.com>
2022-08-31 17:00:04 +0100

    Update demo_mongodb_insert.js

--- demo_mongodb_insert.js
+++ demo_mongodb_insert.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://Victor:<Teodora13>@localhost:27017/";
+var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
