b7b37064545c43fec751278435346fb77c948879
VictorCraciun <craciun.victor.tudor@gmail.com>
2022-09-05 19:40:20 +0100

    Update demo_mongodb_find_fields.js
    v1

--- demo_mongodb_find_fields.js
+++ demo_mongodb_find_fields.js
@@ -4,7 +4,7 @@ var url = "mongodb://127.0.0.1:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  dbo.collection("customers").find({}, { projection: {name: 1, address: 1 } }).toArray(function(err, result) {
+  dbo.collection("customers").find({}, { projection: { _id: 0, name: 1, address: 1 } }).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
